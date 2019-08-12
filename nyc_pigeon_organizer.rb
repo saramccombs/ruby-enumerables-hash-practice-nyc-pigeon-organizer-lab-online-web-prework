22cc4c324fe5598fb094ee33eeacb592653ba04f
Sara McCombs <thesaramccombs@gmail.com>
2019-08-12 02:53:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,10 @@
-def nyc_pigeon_organizer(data)
   #visit each color elements in the hash
   #visit each gender elements in the hash
   #visit lives elements in the hash
   #When you iterate each :color element, you will collect the key-names in an array
   #Check if the key-name already exists, create new if FALSE
   # For each key-name, assign that to key 
+
+def nyc_pigeon_organizer(data)
   p data[:color][:purple][0]
 end
